6001aecefb53e24596ce183ed6c77f0d9617c762
sjgame <107088357+sjgame@users.noreply.github.com>
2022-06-15 11:56:10 +1000

    Added Pickup Counter + Win Condition
    Scripted trigger pickups and win condition to the player controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed = 1.0f;
+    public int pickupCount; //Stores pickup count (variable)
+    private bool wonGame = false; 
+
     void Start()
     {
         //Gets the rigidbody component attached to this game object
         rb = GetComponent<Rigidbody>();
+
+        //Work out how many pickups are in the scene and store in (pickupCount)
+        pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+
+        //Display the pickups to the user
+        CheckPickups();
     }
 
-   
     void FixedUpdate()
+
     {
+        if (wonGame) //disables player controls if (wonGame = true) returns function 
+            return;
+
         //Store the horizontal axis value in a float 
         float moveHorizontal = Input.GetAxis("Horizontal");
         //Store the vertical axis value in a float 
@@ -33,8 +45,31 @@ public class PlayerController : MonoBehaviour
         //if we trigger a pickup, destroy the pickup    
         if (other.gameObject.CompareTag("Pickup"))
         {
+            //Decrement the pickupCount when we collide with a pickup
+            pickupCount -= 1;
+            CheckPickups();
+
             Destroy(other.gameObject);
         }
+
+        //Create a win condition that happens when pickup count == 0
+
     }
 
+    void CheckPickups()
+    {
+        //Display the new pickup count to the player
+        Debug.Log("Pickups Remaining: " + pickupCount);
+        //Check if the pickupCount == 0
+        if (pickupCount == 0)
+        {
+            //if pickupCount == 0, display win message
+            Debug.Log("You Win!");
+            //Remove controls from player
+            wonGame = true;
+            //set the velocity of the rigidBody to 0
+            rb.velocity = Vector3.zero;
+
+        }
+    }
 }
